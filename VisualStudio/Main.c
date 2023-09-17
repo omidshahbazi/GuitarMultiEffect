1649f10893d7e6ad4e5fcfe697d6d8ee810f0035
Omid Shahbazi <sh.omid.m@gmail.com>
2023-09-17 16:02:13 +0330

    Modified   Proteus/GuitarMultiEffect.pdsprj Modified   VisualStudio/GuitarMultiEffect.vcxproj Modified   VisualStudio/GuitarMultiEffect.vcxproj.filters Modified   VisualStudio/Main.c Added      VisualStudio/Utils.h

--- VisualStudio/Main.c
+++ VisualStudio/Main.c
@@ -1,59 +1,81 @@
+#include "Utils.h"
 #include <inttypes.h>
-#include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 #include <util/delay.h>
 
-#define TURN_ON_PORT(Port, Pin) Port |= (1 << Pin)
-#define TURN_OFF_PORT(Port, Pin) Port &= ~(1 << Pin)
+//void run()
+//{
+//	DDRD = 0xff;	// Set Port D for LED output
+//	PORTD = 0x00;	// Clear Portd pins
+//
+//	//ADMUX = (1 << REFS1) | (1 << REFS0) | (0 << MUX0) | (0 << MUX1) | (0 << MUX2) | (0 << MUX3);
+//
+//	ADMUX = 0;
+//	ADCSRA = 0;
+//
+//	//ADMUX |= (1 << MUX0);
+//	//ADMUX |= (1 << MUX2);
+//
+//	ADCSRA |= (1 << ADPS0);
+//	ADCSRA |= (1 << ADPS1);
+//	ADCSRA |= (1 << ADPS2);
+//
+//	ADCSRA |= (1 << ADEN);
+//	ADCSRA |= (1 << ADFR);
+//	ADCSRA |= (1 << ADSC);
+//
+//
+//	double firstADC = getADC();
+//
+//	while (1)	//Forever since it is in  single conversion mode
+//	{
+//		double currADC = getADC();	// Variable to hold ADC  result
+//
+//		if (currADC > firstADC)
+//		{
+//
+//			TURN_ON_PORT(PORTD, 0);
+//		}
+//		else
+//		{
+//			TURN_OFF_PORT(PORTD, 0);
+//		}
+//	}
+//}
 
-double getADC(void) // Read ADC
+void run1()
 {
-	while ((ADCSRA & (1 << ADIF)) == 0); // Wait for the conversion to complete
+	SetPortDMode(true);
 
-	return ADC;
-}
+	BindMUXToADCPin(0);
 
-void run()
-{
-	DDRD = 0xff;	// Set Port D for LED output
-	PORTD = 0x00;	// Clear Portd pins
+	SetADCDivisionFactor(128);
 
-	//ADMUX = (1 << REFS1) | (1 << REFS0) | (0 << MUX0) | (0 << MUX1) | (0 << MUX2) | (0 << MUX3);
-	
-	ADMUX = 0;
-	ADMUX |= (1 << MUX0);
-	ADMUX |= (1 << MUX2);
+	EnableADC();
+	EnableADCFreeRun();
+	StartADC();
 
-	ADCSRA = 0;
-	ADCSRA |= (1 << ADEN);
-	ADCSRA |= (1 << ADPS0);
-	ADCSRA |= (1 << ADPS1);
-	ADCSRA |= (1 << ADFR);
-	ADCSRA |= (1 << ADSC);
+	double firstADC = ReadADCValue();
 
-
-	double firstADC = getADC();
-
-	while (1)	//Forever since it is in  single conversion mode
+	while (1)
 	{
-		double currADC = getADC();	// Variable to hold ADC  result
+		double currADC = ReadADCValue();
 
 		if (currADC > firstADC)
 		{
-
-			TURN_ON_PORT(PORTD, 0);
+			TurnOnPin(PORTD, 0);
 		}
 		else
 		{
-			TURN_OFF_PORT(PORTD, 0);
+			TurnOffPin(PORTD, 0);
 		}
 	}
 }
 
 int main()
 {
-	run();
+	run1();
 
 
 	//DDRC = 0b00000000;
---
+++ VisualStudio/Utils.h
@@ -0,0 +1,202 @@
+#pragma once
+#include <avr/io.h>
+
+#define bool char
+#define true 1
+#define false 0
+
+#define EnableBit(Register, BitIndex) (Register |= (1 << BitIndex))
+#define DisableBit(Register, BitIndex) (Register &= ~(1 << BitIndex))
+#define IsBitEnabled(Register, BitIndex) ((Register & (1 << BitIndex)) != 0)
+
+#define TurnOnPort(Port) (Port = 0xFF)
+#define TurnOffPort(Port) (Port = 0x00)
+
+#define TurnOnPin(Port, PinIndex) EnableBit(Port, PinIndex)
+#define TurnOffPin(Port, PinIndex) DisableBit(Port, PinIndex)
+
+#define SetPortBMode(Writable) (DDRB = (Writable ? 0xFF : 0))
+#define SetPortDMode(Writable) (DDRD = (Writable ? 0xFF : 0))
+#define SetPortCMode(Writable) (DDRC = (Writable ? 0xFF : 0))
+
+#define SetPortBPinMode(PinIndex, Writable) (DDRB = (Writable ? DDRB | (1 << PinIndex) : DDRB & ~(1 << PinIndex))
+#define SetPortDPinMode(PinIndex, Writable) (DDRB = (Writable ? DDRB | (1 << PinIndex) : DDRB & ~(1 << PinIndex))
+#define SetPortCPinMode(PinIndex, Writable) (DDRB = (Writable ? DDRB | (1 << PinIndex) : DDRB & ~(1 << PinIndex))
+
+void BindMUXToADCPin(unsigned int PinIndex)
+{
+	switch (PinIndex)
+	{
+	case 0:
+	{
+		DisableBit(ADMUX, MUX0);
+		DisableBit(ADMUX, MUX1);
+		DisableBit(ADMUX, MUX2);
+		DisableBit(ADMUX, MUX3);
+	} break;
+
+	case 1:
+	{
+		EnableBit(ADMUX, MUX0);
+		DisableBit(ADMUX, MUX1);
+		DisableBit(ADMUX, MUX2);
+		DisableBit(ADMUX, MUX3);
+	} break;
+
+	case 2:
+	{
+		DisableBit(ADMUX, MUX0);
+		EnableBit(ADMUX, MUX1);
+		DisableBit(ADMUX, MUX2);
+		DisableBit(ADMUX, MUX3);
+	} break;
+
+	case 3:
+	{
+		EnableBit(ADMUX, MUX0);
+		EnableBit(ADMUX, MUX1);
+		DisableBit(ADMUX, MUX2);
+		DisableBit(ADMUX, MUX3);
+	} break;
+
+	case 4:
+	{
+		EnableBit(ADMUX, MUX0);
+		DisableBit(ADMUX, MUX1);
+		EnableBit(ADMUX, MUX2);
+		DisableBit(ADMUX, MUX3);
+	} break;
+
+	case 5:
+	{
+		EnableBit(ADMUX, MUX0);
+		EnableBit(ADMUX, MUX1);
+		EnableBit(ADMUX, MUX2);
+		EnableBit(ADMUX, MUX3);
+	} break;
+	}
+}
+
+void EnableUsingAVCC(void)
+{
+	DisableBit(ADMUX, REFS0);
+	DisableBit(ADMUX, REFS1);
+}
+
+void EnableUsingVREF(void)
+{
+	EnableBit(ADMUX, REFS0);
+	DisableBit(ADMUX, REFS1);
+}
+
+void EnableUsingInternal2_56v(void)
+{
+	EnableBit(ADMUX, REFS0);
+	EnableBit(ADMUX, REFS1);
+}
+
+void EnableUsingInternal1_1v(void)
+{
+	DisableBit(ADMUX, REFS0);
+	EnableBit(ADMUX, REFS1);
+}
+
+void EnableADC(void)
+{
+	EnableBit(ADCSRA, ADEN);
+}
+
+void DisableADC(void)
+{
+	DisableBit(ADCSRA, ADEN);
+}
+
+void StartADC(void)
+{
+	EnableBit(ADCSRA, ADSC);
+}
+
+void StopADC(void)
+{
+	DisableBit(ADCSRA, ADSC);
+}
+
+void EnableADCFreeRun(void)
+{
+	EnableBit(ADCSRA, ADFR);
+}
+
+void DisableADCFreeRun(void)
+{
+	DisableBit(ADCSRA, ADFR);
+}
+
+//Must be a power two
+void SetADCDivisionFactor(unsigned int Value)
+{
+	switch (Value)
+	{
+	case 2:
+	{
+		EnableBit(ADCSRA, ADPS0);
+		DisableBit(ADCSRA, ADPS1);
+		DisableBit(ADCSRA, ADPS2);
+	} break;
+
+	case 4:
+	{
+		DisableBit(ADCSRA, ADPS0);
+		EnableBit(ADCSRA, ADPS1);
+		DisableBit(ADCSRA, ADPS2);
+	} break;
+
+	case 8:
+	{
+		EnableBit(ADCSRA, ADPS0);
+		EnableBit(ADCSRA, ADPS1);
+		DisableBit(ADCSRA, ADPS2);
+	} break;
+
+	case 16:
+	{
+		DisableBit(ADCSRA, ADPS0);
+		DisableBit(ADCSRA, ADPS1);
+		EnableBit(ADCSRA, ADPS2);
+	} break;
+
+	case 32:
+	{
+		EnableBit(ADCSRA, ADPS0);
+		DisableBit(ADCSRA, ADPS1);
+		EnableBit(ADCSRA, ADPS2);
+	} break;
+
+	case 64:
+	{
+		DisableBit(ADCSRA, ADPS0);
+		EnableBit(ADCSRA, ADPS1);
+		EnableBit(ADCSRA, ADPS2);
+	} break;
+
+	case 128:
+	{
+		EnableBit(ADCSRA, ADPS0);
+		EnableBit(ADCSRA, ADPS1);
+		EnableBit(ADCSRA, ADPS2);
+	} break;
+	}
+}
+
+unsigned int ReadADCValue()
+{
+	while (!IsBitEnabled(ADCSRA, ADIF));
+
+	return ADC;
+}
+
+unsigned int StartAndReadADCValue()
+{
+	StartADC();
+
+	return ReadADCValue();
+}
