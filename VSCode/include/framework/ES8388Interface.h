02b02af0b275b2df0373c1fbda0c055257dbc867
Omid Shahbazi <sh.omid.m@gmail.com>
2023-10-26 21:23:18 +0330

    Modified   VSCode/include/framework/ES8388Interface.h Modified   VSCode/src/Application.cpp

--- VSCode/include/framework/ES8388Interface.h
+++ VSCode/include/framework/ES8388Interface.h
@@ -30,7 +30,7 @@ public:
 		ES8388Control::Write(ES8388Control::Registers::DACControl21, 0x80); // set internal ADC and DAC use the same LRCK clock, ADC LRCK as internal LRCK
 		ES8388Control::Write(ES8388Control::Registers::DACControl23, 0x00); // vroi=0
 
-		// SetDACVolume(0, 0); // 0db
+		SetDACVolume(0, 0); // 0db
 
 		Log::WriteInfo(TAG, "Setting DAC Output");
 		ES8388Control::Write(ES8388Control::Registers::DACPower, ES8388Control::Values::DACPower_LOUT1_1, ES8388Control::Masks::DACPower_LOUT1);
@@ -51,7 +51,7 @@ public:
 		ES8388Control::Write(ES8388Control::Registers::ADCControl5, 0x02); // ADCFsMode,singel SPEED,RATIO=256
 
 		// ALC for Microphone
-		// SetADCVolume(0, 0);												// 0db
+		SetADCVolume(0, 0);												// 0db
 		ES8388Control::Write(ES8388Control::Registers::ADCPower, 0x09); // Power on ADC, Enable LIN&RIN, Power off MICBIAS, set int1lp to low power mode
 
 		Log::WriteInfo(TAG, "Setting I2S ADC Format");
@@ -66,50 +66,74 @@ public:
 		Log::WriteInfo(TAG, "Setting I2S DAC Bits");
 		ES8388Control::Write(ES8388Control::Registers::DACControl1, ES8388Control::Values::DACControl1_DACWL_100, ES8388Control::Masks::DACControl1_DACWL);
 
-		SetOutputVolume(14.5F);
+		SetOutputVolume(4.5F);
 
-		uint8 prevData = I2CRead(DACRegisters::Control21);
+		uint8 prevData = ES8388Control::Read(ES8388Control::Registers::DACControl21);
 
-		if (Bitwise::IsEnabled(Module, Modules::Line))
+		if (0) // Bitwise::IsEnabled(Module, Modules::Line)
 		{
-			I2CWrite(DACRegisters::Control16, 0x09); // 0x00 audio on LIN1&RIN1,  0x09 LIN2&RIN2 by pass enable
-			I2CWrite(DACRegisters::Control17, 0x50); // left DAC to left mixer enable  and  LIN signal to left mixer enable 0db  : bupass enable
-			I2CWrite(DACRegisters::Control20, 0x50); // right DAC to right mixer enable  and  LIN signal to right mixer enable 0db : bupass enable
-			I2CWrite(DACRegisters::Control21, 0xC0); // enable adc
+			// 0x00 audio on LIN1&RIN1,  0x09 LIN2&RIN2 by pass enable
+			ES8388Control::Write(ES8388Control::Registers::DACControl16, ES8388Control::Values::DACControl16_RMIXSEL_001, ES8388Control::Masks::DACControl16_RMIXSEL);
+			ES8388Control::Write(ES8388Control::Registers::DACControl16, ES8388Control::Values::DACControl16_LMIXSEL_001, ES8388Control::Masks::DACControl16_LMIXSEL);
+
+			// left DAC to left mixer enable  and  LIN signal to left mixer enable 0db  : bupass enable
+			ES8388Control::Write(ES8388Control::Registers::DACControl17, ES8388Control::Values::DACControl17_LI2LO_1, ES8388Control::Masks::DACControl17_LI2LO);
+			ES8388Control::Write(ES8388Control::Registers::DACControl17, ES8388Control::Values::DACControl17_LI2LOVOL_010, ES8388Control::Masks::DACControl17_LI2LOVOL);
+
+			// right DAC to right mixer enable  and  LIN signal to right mixer enable 0db : bupass enable
+			ES8388Control::Write(ES8388Control::Registers::DACControl20, ES8388Control::Values::DACControl20_RI2RO_1, ES8388Control::Masks::DACControl20_RI2RO);
+			ES8388Control::Write(ES8388Control::Registers::DACControl20, ES8388Control::Values::DACControl20_RI2ROVOL_010, ES8388Control::Masks::DACControl20_RI2ROVOL);
+
+			// enable adc
+			ES8388Control::Write(ES8388Control::Registers::DACControl21, ES8388Control::Values::DACControl21_lrck_sel_1, ES8388Control::Masks::DACControl21_lrck_sel);
+			ES8388Control::Write(ES8388Control::Registers::DACControl21, ES8388Control::Values::DACControl21_slrck_1, ES8388Control::Masks::DACControl21_slrck);
 		}
 		else
 		{
-			I2CWrite(DACRegisters::Control21, 0x80); // enable dac
+			ES8388Control::Write(ES8388Control::Registers::DACControl21, 0x80); // enable dac
 		}
 
-		uint8 data = I2CRead(DACRegisters::Control21);
+		uint8 data = ES8388Control::Read(ES8388Control::Registers::DACControl21);
 
 		if (prevData != data)
 		{
 			Log::WriteInfo(TAG, "Resetting State Machine");
 
-			I2CWrite(ChipRegisters::Power, 0xF0); // start state machine
+			// start state machine
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_dac_stm_rst_1, ES8388Control::Masks::ChipPower_dac_stm_rst);
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_adc_stm_rst_1, ES8388Control::Masks::ChipPower_adc_stm_rst);
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_dac_DigPDN_1, ES8388Control::Masks::ChipPower_dac_DigPDN);
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_adc_DigPDN_1, ES8388Control::Masks::ChipPower_adc_DigPDN);
+
 			// I2CWrite(ChipRegisters::Control1, 0x16);
 			// I2CWrite(ChipRegisters::Control2, 0x50);
 			// I2CWrite(ChipRegisters::Control2, 0x50);
-			I2CWrite(ChipRegisters::Power, 0x00); // start state machine
+
+			// start state machine
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_dacVref_PDN_0, ES8388Control::Masks::ChipPower_dacVref_PDN);
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_adcVref_PDN_0, ES8388Control::Masks::ChipPower_adcVref_PDN);
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_DACDLL_PDN_0, ES8388Control::Masks::ChipPower_DACDLL_PDN);
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_ADCDLL_PDN_0, ES8388Control::Masks::ChipPower_ADCDLL_PDN);
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_dac_stm_rst_0, ES8388Control::Masks::ChipPower_dac_stm_rst);
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_adc_stm_rst_0, ES8388Control::Masks::ChipPower_adc_stm_rst);
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_dac_DigPDN_0, ES8388Control::Masks::ChipPower_dac_DigPDN);
+			ES8388Control::Write(ES8388Control::Registers::ChipPower, ES8388Control::Values::ChipPower_adc_DigPDN_0, ES8388Control::Masks::ChipPower_adc_DigPDN);
 		}
 
-		if (Bitwise::IsEnabled(Module, Modules::ADC) || Bitwise::IsEnabled(Module, Modules::Line))
+		// if (Bitwise::IsEnabled(Module, Modules::ADC) || Bitwise::IsEnabled(Module, Modules::Line))
 		{
 			Log::WriteInfo(TAG, "Starting the ADC");
 
-			I2CWrite(ADCRegisters::Power, 0x00);
+			ES8388Control::Write(ES8388Control::Registers::ADCPower, 0x00);
 		}
 
-		if (Bitwise::IsEnabled(Module, Modules::DAC) || Bitwise::IsEnabled(Module, Modules::Line))
+		// if (Bitwise::IsEnabled(Module, Modules::DAC) || Bitwise::IsEnabled(Module, Modules::Line))
 		{
 			Log::WriteInfo(TAG, "Starting the DAC");
 
-			I2CWrite(DACRegisters::Power, 0x3c);
+			ES8388Control::Write(ES8388Control::Registers::DACPower, 0x3c);
 
 			CHECK_CALL(SetMute(false));
-			//- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 		}
 
 		return true;
@@ -137,6 +161,63 @@ public:
 		ES8388Control::Values value = ES8388Control::Read(ES8388Control::Registers::DACControl24, ES8388Control::Masks::DACControl24_LOUT1VOL);
 
 		return ((uint8)value * 1.5F) - 45;
+	}
+
+	static bool SetMute(bool Mute)
+	{
+		Log::WriteInfo(TAG, "Setting the Mute: %i", Mute);
+
+		ES8388Control::Write(
+			ES8388Control::Registers::DACControl3,
+			(Mute ? ES8388Control::Values::DACControl3_DACMute_1 : ES8388Control::Values::DACControl3_DACMute_0),
+			ES8388Control::Masks::DACControl3_DACMute);
+
+		return true;
+	}
+
+	static bool GetMute(void)
+	{
+		return (ES8388Control::Read(ES8388Control::Registers::DACControl3, ES8388Control::Masks::DACControl3_DACMute) == ES8388Control::Values::DACControl3_DACMute_1);
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	static bool SetADCVolume(int32 Volume, int32 Dot)
+	{
+		int32 vol = GetVolume(Volume, Dot);
+
+		ES8388Control::Write(ES8388Control::Registers::ADCControl8, vol);
+		ES8388Control::Write(ES8388Control::Registers::ADCControl9, vol); // ADC Right Volume=0db
+
+		return true;
+	}
+
+	static bool SetDACVolume(int32 Volume, int32 Dot)
+	{
+		int32 vol = GetVolume(Volume, Dot);
+
+		ES8388Control::Write(ES8388Control::Registers::DACControl5, vol);
+		ES8388Control::Write(ES8388Control::Registers::DACControl4, vol);
+
+		return true;
+	}
+
+	static int32 GetVolume(int32 Volume, int32 Dot)
+	{
+		if (Volume < -96 || Volume > 0)
+		{
+			Log::WriteWarning(TAG, "Warning: volume < -96! or > 0!");
+
+			if (Volume < -96)
+				Volume = -96;
+			else
+				Volume = 0;
+		}
+
+		Dot = (Dot >= 5 ? 1 : 0);
+
+		Volume = (-Volume << 1) + Dot;
+
+		return Volume;
 	}
 
 private:
