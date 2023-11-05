202b1e46f68e3c35d65a837ec285785023bdf0f1
Omid Shahbazi <sh.omid.m@gmail.com>
2023-11-05 16:51:38 +0330

    Modified   VSCode/src/Application.cpp

--- VSCode/src/Application.cpp
+++ VSCode/src/Application.cpp
@@ -29,7 +29,7 @@ void Application::Initialize(void)
 	configs.Version = ESP32A1SCodec::Versions::V2974;
 	configs.TransmissionMode = ESP32A1SCodec::TransmissionModes::Both;
 	configs.SampleRate = 44100;
-	configs.BitsPerSample = ES8388::BitsPerSamples::BPS16;
+	configs.BitsPerSample = ES8388::BitsPerSamples::BPS32;
 	configs.ChannelFormat = ESP32A1SCodec::ChannelFormats::SeparatedLeftAndRight;
 	configs.BufferCount = 3;
 	configs.BufferLegth = 300;
