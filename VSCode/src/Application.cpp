2a451017fb22b1a0139bee6f230e7c70acb52733
Omid Shahbazi <sh.omid.m@gmail.com>
2023-11-16 11:12:39 +0330

    Modified   VSCode/src/Application.cpp

--- VSCode/src/Application.cpp
+++ VSCode/src/Application.cpp
@@ -40,7 +40,7 @@ void Application::Initialize(void)
 	configs.Version = ESP32A1SCodec::Versions::V2974;
 	configs.SampleRate = SAMPLE_RATE;
 	configs.BitsPerSample = ESP32A1SCodec::BitsPerSamples::BPS32;
-	configs.ChannelFormat = ESP32A1SCodec::ChannelFormats::LeftAndRight;
+	configs.ChannelFormat = ESP32A1SCodec::ChannelFormats::OnlyLeft;
 	configs.BufferCount = 3;
 	configs.BufferLength = 300;
 	configs.InputMode = ESP32A1SCodec::InputModes::LineL;
@@ -59,9 +59,9 @@ void Application::Initialize(void)
 	// CreateEffect<OverdriveEffect>(m_Effects);
 
 	Task::Create(
-		[this]()
+		[&]()
 		{
-			this->PassthroughTask();
+			PassthroughTask();
 		},
 		1, 10);
 }
@@ -72,11 +72,10 @@ void Application::PassthroughTask(void)
 
 	int32 *ioBuffer = Memory::Allocate<int32>(SAMPLE_COUNT);
 	double *processBufferL = Memory::Allocate<double>(FRAME_LENGTH);
-	double *processBufferR = Memory::Allocate<double>(FRAME_LENGTH);
 
-	double sumL = 0, sumR = 0;
-	uint16 count = 0;
-	float nextTime = 0;
+	// double sumL = 0;
+	// uint16 count = 0;
+	// float nextTime = 0;
 
 	while (true)
 	{
@@ -85,41 +84,27 @@ void Application::PassthroughTask(void)
 		if (m_Mute)
 		{
 			Memory::Set(processBufferL, 0, FRAME_LENGTH);
-			// Memory::Set(processBufferR, 0, FRAME_LENGTH);
 		}
 		else
 		{
-			// for (uint16 i = 0; i < FRAME_LENGTH; ++i)
-			// 	CONVERT_TO_24_AND_NORMALIZED_DOUBLE(processBufferL, i, ioBuffer, 0);
-
-			// for (uint16 i = 0; i < FRAME_LENGTH; ++i)
-			// 	CONVERT_TO_24_AND_NORMALIZED_DOUBLE(processBufferR, i, ioBuffer, 1);
-
 			for (uint16 i = 0; i < FRAME_LENGTH; ++i)
 			{
 				CONVERT_TO_24_AND_NORMALIZED_DOUBLE(processBufferL, i, ioBuffer, 0);
 
-				sumL += processBufferL[i];
+				// sumL += processBufferL[i];
 			}
 
-			for (uint16 i = 0; i < FRAME_LENGTH; ++i)
-			{
-				CONVERT_TO_24_AND_NORMALIZED_DOUBLE(processBufferR, i, ioBuffer, 1);
+			// count += FRAME_LENGTH;
 
-				sumR += processBufferR[i];
-			}
+			// if (nextTime < Time::Now())
+			// {
+			// 	nextTime += 1;
 
-			count += FRAME_LENGTH;
+			// 	Log::WriteError("Avg: %f", sumL / count);
 
-			if (nextTime < Time::Now())
-			{
-				nextTime += 1;
-
-				// Log::WriteError("Avg L: %f, R: %f", sumL / count, sumR / count);
-
-				sumL = sumR = 0;
-				count = 0;
-			}
+			// 	sumL = 0;
+			// 	count = 0;
+			// }
 		}
 
 		for (Effect *effect : m_Effects)
@@ -128,14 +113,10 @@ void Application::PassthroughTask(void)
 		for (uint16 i = 0; i < FRAME_LENGTH; ++i)
 			SCALE_TO_24_AND_SATURATED_32(processBufferL, i, ioBuffer, 0);
 
-		for (uint16 i = 0; i < FRAME_LENGTH; ++i)
-			SCALE_TO_24_AND_SATURATED_32(processBufferR, i, ioBuffer, 1);
-
-		ESP32A1SCodec::Write(ioBuffer, SAMPLE_COUNT);
+		ESP32A1SCodec::Write(ioBuffer, SAMPLE_COUNT, 20);
 	}
 
 	Memory::Deallocate(processBufferL);
-	// Memory::Deallocate(processBufferR);
 	Memory::Deallocate(ioBuffer);
 
 	Task::Delete();
