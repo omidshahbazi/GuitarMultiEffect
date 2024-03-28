c5ea9dd43e884e49138da75214f578680bafdd3d
Omid Shahbazi <sh.omid.m@gmail.com>
2024-03-28 09:42:56 +0330

    Modified   .gitmodules Modified   VSCode/.gitignore Missing    VSCode/.vscode/extensions.json Missing    VSCode/.vscode/settings.json Missing    VSCode/include/README Missing    VSCode/lib/README Missing    VSCode/platformio.ini Modified   VSCode/src/main.cpp Missing    VSCode/test/README Added      VSCode/.vscode/.cortex-debug.peripherals.state.json Added      VSCode/.vscode/.cortex-debug.registers.state.json Added      VSCode/.vscode/STM32H750x.svd Added      VSCode/.vscode/c_cpp_properties.json Added      VSCode/.vscode/launch.json Added      VSCode/.vscode/tasks.json Added      VSCode/Makefile

--- VSCode/src/main.cpp
+++ VSCode/src/main.cpp
@@ -1,13 +1,41 @@
-#include "Application.h"
-#include <Arduino.h>
+// #include "Application.h"
+// #include <Arduino.h>
 
-Application g_App;
+// Application g_App;
 
-void setup()
+// void setup()
+// {
+// 	g_App.Initialize();
+// }
+
+// void loop()
+// {
+// }
+
+#include "daisy_seed.h"
+// #include "daisysp.h"
+
+using namespace daisy;
+using namespace daisysp;
+
+DaisySeed hw;
+
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
-	g_App.Initialize();
+	for (size_t i = 0; i < size; i++)
+	{
+		out[0][i] = in[0][i];
+		out[1][i] = in[1][i];
+	}
 }
 
-void loop()
+int main(void)
 {
+	hw.Init();
+	hw.SetAudioBlockSize(4); // number of samples handled per callback
+	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+	hw.StartAudio(AudioCallback);
+	while (1)
+	{
+	}
 }
