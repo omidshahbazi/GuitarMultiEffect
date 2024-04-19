91943dfcdee27f008ede66b6ef819bd7fde9212e
Omid Shahbazi <sh.omid.m@gmail.com>
2024-04-19 08:11:28 +0330

    Modified   VSCode/include/Effects/FlangerEffect.h Modified   VSCode/include/framework

--- VSCode/include/Effects/FlangerEffect.h
+++ VSCode/include/Effects/FlangerEffect.h
@@ -19,33 +19,33 @@ public:
 		  m_DepthPot(nullptr),
 		  m_RatePot(nullptr)
 	{
-		// m_FeedbackPot = ControlManager->CreatePotentiometer("Feedback", GPIOPins::Pin4);
-		// m_FeedbackPot->SetOnChangedListener(
-		// 	[&](float value)
-		// 	{
-		// 		m_Flanger.SetFeedback(value);
-		// 	});
+		m_FeedbackPot = ControlManager->CreatePotentiometer("Feedback", GPIOPins::Pin4);
+		m_FeedbackPot->SetOnChangedListener(
+			[&](float value)
+			{
+				m_Flanger.SetFeedback(value);
+			});
 
-		// m_WetRatePot = ControlManager->CreatePotentiometer("Wet Rate", GPIOPins::Pin13);
-		// m_WetRatePot->SetOnChangedListener(
-		// 	[&](float value)
-		// 	{
-		// 		m_Flanger.SetWetRate(value);
-		// 	});
+		m_WetRatePot = ControlManager->CreatePotentiometer("Wet Rate", GPIOPins::Pin13);
+		m_WetRatePot->SetOnChangedListener(
+			[&](float value)
+			{
+				m_Flanger.SetWetRate(value);
+			});
 
-		// m_DepthPot = ControlManager->CreatePotentiometer("Depth", GPIOPins::Pin14);
-		// m_DepthPot->SetOnChangedListener(
-		// 	[&](float value)
-		// 	{
-		// 		m_Flanger.SetDepth(Math::Lerp(0.0, Flanger<T>::MAX_DEPTH, value));
-		// 	});
+		m_DepthPot = ControlManager->CreatePotentiometer("Depth", GPIOPins::Pin14);
+		m_DepthPot->SetOnChangedListener(
+			[&](float value)
+			{
+				m_Flanger.SetDepth(Math::Lerp(0.0, Flanger<T>::MAX_DEPTH, value));
+			});
 
-		// m_RatePot = ControlManager->CreatePotentiometer("Rate", GPIOPins::Pin15);
-		// m_RatePot->SetOnChangedListener(
-		// 	[&](float value)
-		// 	{
-		// 		m_Flanger.SetRate(Math::Lerp(0.01, 4, value));
-		// 	});
+		m_RatePot = ControlManager->CreatePotentiometer("Rate", GPIOPins::Pin15);
+		m_RatePot->SetOnChangedListener(
+			[&](float value)
+			{
+				m_Flanger.SetRate(Math::Lerp(0.01, 4, value));
+			});
 	}
 
 protected:
