186f8e5c3c57800b28ab3f0ef4324b9d61af4546
Omid Shahbazi <sh.omid.m@gmail.com>
2024-05-16 11:29:53 +0330

    Modified   VSCode/include/ControlManager.h

--- VSCode/include/ControlManager.h
+++ VSCode/include/ControlManager.h
@@ -12,14 +12,11 @@
 #include "framework/DSP/Controls/Potentiometer.h"
 #include "framework/DSP/Controls/RotaryButton.h"
 
-#define PROCESS_RATE 100
-
 class ControlManager
 {
 public:
 	ControlManager(IHAL *HAL)
-		: m_HAL(HAL),
-		  m_UsedGPIOs{}
+		: m_Factory(HAL)
 	{
 	}
 
@@ -30,122 +27,66 @@ public:
 
 	SingleLED *CreateSingleLED(const char *Name, GPIOPins Pin)
 	{
-		CheckIfGPIOIsUsed((uint8)Pin);
-
-		MarkGPIOAsUsed((uint8)Pin);
-
 		Log::WriteInfo("Controls", "%s: LED %i", Name, Pin);
 
-		return m_Factory.Create<SingleLED>(m_HAL, (uint8)Pin, PROCESS_RATE);
+		return m_Factory.CreateSingleLED((uint8)Pin);
 	}
 	DualLED *CreateDualLED(const char *Name, GPIOPins RedPin, GPIOPins GreenPin)
 	{
-		CheckIfGPIOIsUsed((uint8)RedPin);
-		CheckIfGPIOIsUsed((uint8)GreenPin);
-
-		MarkGPIOAsUsed((uint8)RedPin);
-		MarkGPIOAsUsed((uint8)GreenPin);
-
 		Log::WriteInfo("Controls", "%s: LED %i", Name, RedPin);
 		Log::WriteInfo("Controls", "%s: LED %i", Name, GreenPin);
 
-		return m_Factory.Create<DualLED>(m_HAL, (uint8)RedPin, (uint8)GreenPin, PROCESS_RATE);
+		return m_Factory.CreateDualLED((uint8)RedPin, (uint8)GreenPin);
 	}
 	TripleLED *CreateTripleLED(const char *Name, GPIOPins RedPin, GPIOPins GreenPin, GPIOPins BluePin)
 	{
-		CheckIfGPIOIsUsed((uint8)RedPin);
-		CheckIfGPIOIsUsed((uint8)GreenPin);
-		CheckIfGPIOIsUsed((uint8)BluePin);
-
-		MarkGPIOAsUsed((uint8)RedPin);
-		MarkGPIOAsUsed((uint8)GreenPin);
-		MarkGPIOAsUsed((uint8)BluePin);
-
 		Log::WriteInfo("Controls", "%s: LED %i", Name, RedPin);
 		Log::WriteInfo("Controls", "%s: LED %i", Name, GreenPin);
 		Log::WriteInfo("Controls", "%s: LED %i", Name, BluePin);
 
-		return m_Factory.Create<TripleLED>(m_HAL, (uint8)RedPin, (uint8)GreenPin, (uint8)BluePin, PROCESS_RATE);
+		return m_Factory.CreateTripleLED((uint8)RedPin, (uint8)GreenPin, (uint8)BluePin);
 	}
 
 	Button *CreateButton(const char *Name, GPIOPins Pin)
 	{
-		CheckIfGPIOIsUsed((uint8)Pin);
-
-		MarkGPIOAsUsed((uint8)Pin);
-
 		Log::WriteInfo("Controls", "%s: Button %i", Name, Pin);
 
-		return m_Factory.Create<Button>(m_HAL, (uint8)Pin, PROCESS_RATE);
+		return m_Factory.CreateButton((uint8)Pin);
 	}
 
 	Switch *CreateSwitch(const char *Name, GPIOPins Pin)
 	{
-		CheckIfGPIOIsUsed((uint8)Pin);
-
-		MarkGPIOAsUsed((uint8)Pin);
-
 		Log::WriteInfo("Controls", "%s: Switch %i", Name, Pin);
 
-		return m_Factory.Create<Switch>(m_HAL, (uint8)Pin, PROCESS_RATE);
+		return m_Factory.CreateSwitch((uint8)Pin);
 	}
 
 	Potentiometer *CreatePotentiometer(const char *Name, AnalogPins Pin)
 	{
-		CheckIfGPIOIsUsed((uint8)Pin);
-
-		MarkGPIOAsUsed((uint8)Pin);
-
 		Log::WriteInfo("Controls", "%s: Pot %i", Name, Pin);
 
-		return m_Factory.Create<Potentiometer>(m_HAL, (uint8)Pin, PROCESS_RATE, false);
+		return m_Factory.CreatePotentiometer((uint8)Pin, false);
 	}
 
 	Rotary *CreateRotary(const char *Name, GPIOPins LeftPin, GPIOPins RightPin)
 	{
-		CheckIfGPIOIsUsed((uint8)LeftPin);
-		CheckIfGPIOIsUsed((uint8)RightPin);
-
-		MarkGPIOAsUsed((uint8)LeftPin);
-		MarkGPIOAsUsed((uint8)RightPin);
-
 		Log::WriteInfo("Controls", "%s: Rotary %i", Name, LeftPin);
 		Log::WriteInfo("Controls", "%s: Rotary %i", Name, RightPin);
 
-		return m_Factory.Create<Rotary>(m_HAL, (uint8)LeftPin, (uint8)RightPin, PROCESS_RATE);
+		return m_Factory.CreateRotary((uint8)LeftPin, (uint8)RightPin);
 	}
 
 	RotaryButton *CreateRotaryButton(const char *Name, GPIOPins LeftPin, GPIOPins RightPin, GPIOPins ButtonPin)
 	{
-		CheckIfGPIOIsUsed((uint8)LeftPin);
-		CheckIfGPIOIsUsed((uint8)RightPin);
-		CheckIfGPIOIsUsed((uint8)ButtonPin);
-
-		MarkGPIOAsUsed((uint8)LeftPin);
-		MarkGPIOAsUsed((uint8)RightPin);
-		MarkGPIOAsUsed((uint8)ButtonPin);
-
 		Log::WriteInfo("Controls", "%s: RotaryButton %i", Name, LeftPin);
 		Log::WriteInfo("Controls", "%s: RotaryButton %i", Name, RightPin);
 		Log::WriteInfo("Controls", "%s: RotaryButton %i", Name, ButtonPin);
 
-		return m_Factory.Create<RotaryButton>(m_HAL, (uint8)LeftPin, (uint8)RightPin, (uint8)ButtonPin, PROCESS_RATE);
+		return m_Factory.CreateRotaryButton((uint8)LeftPin, (uint8)RightPin, (uint8)ButtonPin);
 	}
 
 private:
-	void CheckIfGPIOIsUsed(uint8 Pin)
-	{
-		ASSERT(!m_UsedGPIOs[Pin], "GPIOPin%i is already in use", Pin);
-	}
-	void MarkGPIOAsUsed(uint8 Pin)
-	{
-		m_UsedGPIOs[Pin] = true;
-	}
-
-private:
-	IHAL *m_HAL;
-	ControlFactory m_Factory;
-	bool m_UsedGPIOs[(uint8)GPIOPins::COUNT];
+	ControlFactory<31, 100> m_Factory;
 };
 
 #endif
